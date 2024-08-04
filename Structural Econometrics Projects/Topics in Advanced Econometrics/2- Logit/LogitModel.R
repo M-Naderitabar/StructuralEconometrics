ec0c2d034e1cdd2106581b81e1ff74dd0a59ac34
Mohammad Naderitabar <141520112+GuidosOfLife@users.noreply.github.com>
2024-08-04 15:32:42 +0200

    logit done

--- Structural Econometrics Projects/Topics in Advanced Econometrics/2- Logit/LogitModel.R
+++ Structural Econometrics Projects/Topics in Advanced Econometrics/2- Logit/LogitModel.R
@@ -12,4 +12,23 @@ data_dfidx <- dfidx(data_df, shape = 'wide', choice = 'mode', varying = 3:10)
 model_2a <- mlogit(formula = mode ~ cost | 1 | time,
                    data = data_dfidx)
 
-summary(model_2a)
+data_df_counter <- data_df %>%
+  mutate(time.bus = 0.8 * time.bus)
+## Convert counterfactual data to dfidx format
+data_counter_dfidx <- dfidx(data_df_counter, shape = 'wide',
+                            choice = 'mode', varying = 3:10)
+
+
+agg_choices_obs <- predict(model_2a, newdata = data_dfidx)
+## Calculate aggregate choices using counterfactual data
+agg_choices_counter <- predict(model_2a, newdata = data_counter_dfidx)
+## Calculate difference between aggregate choices
+colSums(agg_choices_counter - agg_choices_obs)
+
+## Calculate log-sum values using observed data
+logsum_obs <- logsum(model_2a, data = data_dfidx)
+## Calculate log-sum values using counterfactual data
+logsum_counter <- logsum(model_2a, data = data_counter_dfidx)
+## Calculate change in consumer surplus from subsidy
+sum((logsum_counter - logsum_obs) / -coef(model_2a)[4])
+
